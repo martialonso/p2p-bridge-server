d814f98957be87fb3496fe411043cd0bca5dcd18
Marti Alonso <martialonso11@gmail.com>
2021-05-03 02:49:56 +0200

    Re-organize project for server and desktop control

---
+++ control-desktop-app/src/main/java/me.martiii.p2pbridgecontrol/P2PBridgeControl.java
@@ -0,0 +1,11 @@
+package me.martiii.p2pbridgecontrol;
+
+public class P2PBridgeControl {
+    public static void main(String[] args) {
+        new P2PBridgeControl();
+    }
+
+    public P2PBridgeControl() {
+
+    }
+}
